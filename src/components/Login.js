9002b33fac94fe358997963e84b856f226dadab8
Ahmad Arrizki Alhaidar <111831984+AhmadArrizki@users.noreply.github.com>
2024-05-28 01:27:55 +0700

    Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [msg, setMsg] = useState ('');
+    const [msg, setMsg] = useState('');
     const navigate = useNavigate();
 
     const Auth = async(e) =>{
@@ -15,7 +15,7 @@ const Login = () => {
                 email: email,
                 password: password,
             });
-            navigate.push("/dashboard");
+            navigate("/dashboard");
         } catch (error) {
             if(error.response){
                 setMsg(error.response.data.msg);
@@ -23,37 +23,36 @@ const Login = () => {
         }
     }
 
-  return (
-    <section class="hero has-background-grey-light is-fullheight is-fullwidth">
-      <div class="hero-body">
-        <div class="container">
-          <div className="columns is-centered">
-            <div className="column is-4-desktop">
-                <form onSubmit={ Auth } className="box">
-                <p className="has-text-centered">{msg}</p>
-                    <div className="field mt-5">
-                        <label className="label">Email or Username</label>
-                        <div className="controls">
-                            <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    return (
+        <section className="hero is-fullheight" style={{ background: '#0094FF', minHeight: '100vh' }}>
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                            <form onSubmit={Auth} className="box" style={{ background: 'white', padding: '20px', borderRadius: '10px' }}>
+                                <p className="has-text-centered">{msg}</p>
+                                <div className="field">
+                                    <label className="label" style={{ color: 'black' }}>Email or Username</label>
+                                    <div className="control">
+                                        <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} style={{ background: 'white', borderColor: 'black', color: 'black' }}/>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <label className="label" style={{ color: 'black' }}>Password</label>
+                                    <div className="control">
+                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} style={{ background: 'white', borderColor: 'black', color: 'black', }}/>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <button className="button is-success is-fullwidth" style={{ background: '#0094FF', color: 'white'}}>Login</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                    <div className="field mt-5">
-                        <label className="label">Password</label>
-                        <div className="controls">
-                            <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="field mt-5">
-                        <button className="button is-success is-fullwidth">Login</button>
-                    </div>
-                </form>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
 
-export default Login
- 
+export default Login;
